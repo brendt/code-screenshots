dc8944e2836e6b27246c868c7bd181ebde8623bf
Brent Roose <brent@spatie.be>
2018-06-15 15:47:09 +0200

    Different formats

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,15 @@ const buttons = document.querySelectorAll('.actions > button');
 
 for (let button of buttons) {
     button.addEventListener('click', () => {
-        const element = document.querySelector(button.getAttribute('data-selector'));
+        const parent = document.querySelector(button.getAttribute('data-selector'));
 
         const filename = button.getAttribute('data-filename');
+
+        const element = parent.querySelector('pre');
+
+        if (button.getAttribute('data-class')) {
+            parent.classList.add(button.getAttribute('data-class'));
+        }
 
         html2canvas(element).then(canvas => {
             const link = document.createElement('a');
@@ -15,6 +21,10 @@ for (let button of buttons) {
             link.href = canvas.toDataURL();
 
             link.click();
+
+            if (button.getAttribute('data-class')) {
+                parent.classList.remove(button.getAttribute('data-class'));
+            }
         });
     });
 }
